ea061b46bca242e12cd304db07c5b6ec9fac1d0d
Aryan Agrawal <144577656+Strong02coder@users.noreply.github.com>
2025-02-24 20:02:31 +0530

    Refactor FeaturedCourses component to use BackgroundGradient for enhanced styling and layout

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,6 +1,8 @@
+'use client'
 import React from 'react'
 import courseData from "../data/music_courses.json"
 import Link from 'next/link'
+import { BackgroundGradient } from "./ui/background-gradient"
 
 interface Course {
   id: number,
@@ -13,7 +15,7 @@ interface Course {
 }
 
 const FeaturedCourses = () => {
-  const featuredCourses = courseData.courses.filter((course: Course) => course.isFeatured);
+  const featuredCourses = courseData.courses.filter((course:Course) => course.isFeatured)
 
 	return (
 		<div className='py-12 bg-gray-900'>
@@ -25,19 +27,17 @@ const FeaturedCourses = () => {
       </div>
       <div className='mt-10'>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center'>
-          {featuredCourses.map((course:Course) => (
-            <div key={course.id} className='bg-white rounded-lg shadow-lg p-6'>
-              <div className='flex items-center justify-between'>
-                <div className='text-sm text-gray-600'>{course.instructor}</div>
-                <div className='text-sm text-gray-600'>{course.price}</div>
-              </div>
-              <div className='mt-4'>
-                <h3 className='text-lg font-semibold text-gray-800'>{course.title}</h3>
-                <p className='mt-2 text-sm text-gray-600'>{course.description}</p>
-              </div>
-              <div className='mt-6'>
-                <Link href={`/courses/${course.slug}`} className='px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200'>View Course</Link>
-              </div>
+          {featuredCourses.map((course: Course) => (
+            <div key={course.id} className='flex justify-center'>
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
+                  <Link href={`/courses/${course.slug}`}>
+                    Learn More
+                  </Link>
+                </div>
+              </BackgroundGradient>
             </div>
           ))}
         </div>
